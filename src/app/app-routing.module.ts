48c9774fc93971105c1e3be84360e2646cf30721
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-03 23:26:18 +0200

    add routing and home module.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+import {ExtraOptions, RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+const config: ExtraOptions = {
+  scrollPositionRestoration: 'top',
+  onSameUrlNavigation: 'reload',
+  anchorScrolling: 'enabled',
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
