9eac4f3d9181366cf610c0a02a8b91ad0477d476
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-07-19 11:28:54 +0200

    jest update.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
-require('jest-preset-angular/ngcc-jest-processor');
+const {pathsToModuleNameMapper} = require('ts-jest');
+const {paths} = require('./tsconfig.json').compilerOptions;
+
+globalThis.ngJest = {
+  skipNgcc: false,
+  tsconfig: '<rootDir>/tsconfig.spec.json',
+};
 
 module.exports = {
   preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
   setupFilesAfterEnv: [
     '<rootDir>/src/setupJest.ts'
   ], // setup env file
@@ -17,6 +24,7 @@ module.exports = {
     },
   }, // need to be available in all test environments.
   moduleNameMapper: {
+    ...pathsToModuleNameMapper(paths, {prefix: '<rootDir>'}),
     '^@App/(.*)$': '<rootDir>/src/app/$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
@@ -30,7 +38,10 @@ module.exports = {
   // Do not change with jest-circus (jest 27.1.1)
   // a lot of tests won't success (Error: Test functions cannot both take a 'done' callback...)
   testRunner : "jest-jasmine2",
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
   transformIgnorePatterns: [
-    'node_modules/',
+    'node_modules/(?!.*\\.mjs$|preact|@agm)',
   ],
 };
